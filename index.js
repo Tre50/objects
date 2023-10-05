88ac158602378d1f1baa9b48bf31cf0367dcc0eb
Tremayne Johnson <tremayne4@gmail.com>
2023-10-05 16:54:32 -0400

    objects

---
+++ index.js
@@ -0,0 +1,20 @@
+const person = { //declare a new object
+    firstName: "Todd",
+    age: 28
+
+}
+//print the person name
+console.log(person.firstName)
+person.age++
+
+console.log(person.age)
+
+person.lastName = "Albert"  // add a new propertyto the object
+/*
+const person = {firstName: "Todd"}
+age:29
+lastName: "Albert"
+} 
+*/ 
+
+console.log(person.lastName)
